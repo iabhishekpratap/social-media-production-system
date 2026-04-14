7a7d27f8c308f5f7b72931517aae29da510bf762
abhishek-pratap <iapsingh@outlook.com>
2026-04-14 10:37:46 +0000

    add health check endpoint to analytics service

--- analytics_service/analytics/urls.py
+++ analytics_service/analytics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RecordWatchTimeView
+from .views import health_check
 
 urlpatterns = [
     path('record/', RecordWatchTimeView.as_view(), name='record-watch-time'),
+    path('health/', health_check, name='health_check'),
 ]
--- analytics_service/analytics/views.py
+++ analytics_service/analytics/views.py
@@ -1,8 +1,19 @@
 from rest_framework import generics, permissions
 from .models import WatchTime
 from .serializers import WatchTimeSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({
+        "status": "ok",
+        "service": "analytics_service"
+    })
 
 class RecordWatchTimeView(generics.CreateAPIView):
     queryset = WatchTime.objects.all()
     serializer_class = WatchTimeSerializer
     permission_classes = [permissions.AllowAny]
+
+
