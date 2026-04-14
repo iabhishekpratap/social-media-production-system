339e3d00709f20ac09bc7f23306045981e3819b6
abhishek-pratap <iapsingh@outlook.com>
2026-04-14 05:51:26 +0000

    implement health check endpoint for users service

--- users_service/users/views.py
+++ users_service/users/views.py
@@ -3,7 +3,18 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 from .serializers import UserRegistrationSerializer
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = UserRegistrationSerializer
+
+
+@api_view(['GET'])
+def health_check(request):
+    return Response({
+        "status": "ok",
+        "service": "users_service"
+    })
