a9494a204a6e80fbdc9902339900659829077a3c
abhishek-pratap <iapsingh@outlook.com>
2026-04-22 05:20:14 +0000

    migration script for database changes migration yml file to hit migration modified docker-compose to up application after migration is successful

--- infra/scripts/migration.sh
+++ infra/scripts/migration.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-COMPOSE_FILES="-f docker-compose.migrate.yml"
-
-docker compose $COMPOSE_FILES run --rm users_service_migrate
+ENV="--env-file /home/abhishek/prod/social-media/.env"
+COMPOSE_FILES="-f /home/abhishek/prod/social-media/docker-compose.migrate.yml"
 
 set -e  # stop on error
 
@@ -11,13 +10,13 @@ echo "Starting Database Migrations"
 echo "=============================="
 
 echo "Running Users Service migrations..."
-docker compose $COMPOSE_FILES run --rm users_service_migrate
+docker compose $ENV $COMPOSE_FILES run --rm users_service_migrate
 
 echo "Running Video Service migrations..."
-docker compose $COMPOSE_FILES run --rm video_service_migrate
+docker compose $ENV $COMPOSE_FILES run --rm video_service_migrate
 
 echo "Running Analytics Service migrations..."
-docker compose $COMPOSE_FILES run --rm analytics_service_migrate
+docker compose $ENV $COMPOSE_FILES run --rm analytics_service_migrate
 
 echo "=============================="
 echo "All migrations completed successfully!"
